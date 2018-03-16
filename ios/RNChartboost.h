3ff61e51c58e7cb345e51d088096270b293c1ba1
KjellConnelly <kjellapps@gmail.com>
2018-03-16 13:57:27 -0700

    Next Version

--- ios/RNChartboost.h
+++ ios/RNChartboost.h
@@ -5,14 +5,20 @@
 //  Copyright © 2017 POP POP LLC. All rights reserved.
 //
 
-#if __has_include("RCTBridgeModule.h")
-    #import "RCTBridgeModule.h"
-    #import "RCTConvert.h"
-    #import "RCTEventEmitter.h"
+
+
+#if __has_include(<React/RCTBridgeModule.h>)
+  #import <React/RCTBridgeModule.h>
+  #import <React/RCTConvert.h>
+  #import <React/RCTEventEmitter.h>
+#elif __has_include("RCTBridgeModule.h")
+  #import "RCTBridgeModule.h"
+  #import "RCTConvert.h"
+  #import "RCTEventEmitter.h"
 #else
-    #import <React/RCTBridgeModule.h>
-    #import <React/RCTConvert.h>
-    #import <React/RCTEventEmitter.h>
+  #import "RCTBridgeModule.h"
+  #import "RCTConvert.h"
+  #import "RCTEventEmitter.h"
 #endif
 
 #import <UIKit/UIKit.h>
